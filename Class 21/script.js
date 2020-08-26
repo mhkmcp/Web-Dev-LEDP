7d0390ebd3c8ac754f17340dabe784b01478b801
mhkmcp <mhkmcp@yahoo.com>
2020-08-26 14:27:14 +0600

    Added Class 21: JavaScript 1

---
+++ Class 21/script.js
@@ -0,0 +1,14 @@
+document.write("Hola");
+document.getElementById('head').innerHTML = "Hola";
+
+document.getElementById('head1').innerHTML = "My age is: " + 50;
+// window.alert("LEDP");
+
+console.log("Your age is: " + 32);
+
+var name = "HK", age = 25;
+document.write(name);
+
+var a = 30, b = 20, c;
+c = a + b;
+document.write("<br>" + c);
