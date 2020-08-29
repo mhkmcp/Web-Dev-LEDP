19670cc8b6746698b1f48437506e9d6d18748809
mhkmcp <mhkmcp@yahoo.com>
2020-08-29 20:04:43 +0600

    Added Class 24: JavaScript 4

---
+++ Class 24/script.js
@@ -0,0 +1,13 @@
+var str = "LEDPE";
+var len = str.length;
+
+// document.write(len);
+// document.write(str.indexOf('D'));
+// document.write(str.lastIndexOf('E'));
+// document.write(str.search('E'));
+// document.write(str.slice(1, 4));
+document.write(str.replace('D', "HK"));
+
+// toUpperCase()
+// toLowerCase() 
+// concate ()
